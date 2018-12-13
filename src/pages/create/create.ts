b9bcfabe253ebaf67400c537d990f938f54d939c
yanqian <qianyan@thoughtworks.com>
2018-12-13 14:03:44 +0800

    feat(lesson 3): init create new wallet

---
+++ src/pages/create/create.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+@IonicPage()
+@Component({
+  selector: 'page-create',
+  templateUrl: 'create.html',
+})
+export class CreatePage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad CreatePage');
+  }
+
+}
